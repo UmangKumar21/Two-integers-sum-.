4eaebf76dfd2422bc0cbce74520274cf38f634aa
Umang kumar <112698874+UmangKumar21@users.noreply.github.com>
2022-11-09 00:50:34 +0530

    Create TwoIntegerSum.java

---
+++ TwoIntegerSum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Hello{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int A = sc.nextInt();
+    sc.next().charAt(0);
+    int B = sc.nextInt();
+    System.out.println(A+B);
+  }
+}
